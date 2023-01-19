451d62584668365c30fa9efc4c4b1fe5ac08b093
james-leonard <merrymerry1320@gmail.com>
2023-01-19 08:12:37 +0100

    first deploy stage5

--- djbar/urls.py
+++ djbar/urls.py
@@ -14,11 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,  # include
+from barstore import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('barstore/', include('barstore.urls')),
-    path('accounts/', include('registration.backends.default.urls')),
+    path('', views.home, name='home'),
+    path('list_item/', views.list_item, name='list_item'),
+    path('add_category/', views.add_category, name='add_category'),
+    path('add_items/', views.add_items, name='add_items'),
+    path('update_items/<str:pk>/', views.update_items, name="update_items"),
+    path('delete_items/<str:pk>/', views.delete_items, name="delete_items"),
+    path('stock_detail/<str:pk>/', views.stock_detail, name="stock_detail"),
+    path('issue_items/<str:pk>/', views.issue_items, name="issue_items"),
+    path('receive_items/<str:pk>/', views.receive_items, name="receive_items"),
+    path('damage_items/<str:pk>/', views.damage_items, name="damage_items"),
+    path('reorder_level/<str:pk>/', views.reorder_level, name="reorder_level"),
+    path('list_history/', views.list_history, name='list_history'),
+    # path('barstore/', include('barstore.urls')),
+    # path('accounts/', include('registration.backends.default.urls')),
 ]
